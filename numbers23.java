3f89f846b85f4dcd83815a7972aa46f2270971a9
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-02 17:01:11 +0700

    update percobaan 3

--- numbers23.java
+++ numbers23.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class numbers23 {
     public static void main(String[] args) {
         
@@ -5,5 +7,12 @@ public class numbers23 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
