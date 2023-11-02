8de25da4614e38ee9884a0b63269db9fa84538f5
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-02 15:29:24 +0700

    update percobaan 1 untuk nomer 10

--- bioskop23.java
+++ bioskop23.java
@@ -21,5 +21,11 @@ public class bioskop23 {
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }
