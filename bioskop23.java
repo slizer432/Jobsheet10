440e2489816576f3263d6bb230f0f057bff8ebda
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-02 15:09:04 +0700

    update percobaan 2 untuk nomer 6

--- bioskop23.java
+++ bioskop23.java
@@ -1,6 +1,6 @@
 public class bioskop23 {
     public static void main(String[] args) {
-        String[][]  penonton = new String[4][2];
+        String[][] penonton = new String[4][2];
 
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
@@ -12,8 +12,9 @@ public class bioskop23 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
