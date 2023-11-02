b7fccbab1f0d586af34ae6e925d38357e7bf4f25
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-02 16:38:35 +0700

    update percobaan 2

--- bioskopWithScanner23.java
+++ bioskopWithScanner23.java
@@ -5,23 +5,63 @@ public class bioskopWithScanner23 {
         Scanner sc = new Scanner(System.in);
         int baris, kolom;
         String nama, next;
+        int menu;
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("1. Input penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("opsi: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+            switch (menu) {
+                case 1:while (true) {
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+                    
+                    if (baris > penonton.length || kolom > penonton[0].length) {
+                        System.out.println("Kursi tidak tersedia");
+                        continue;
+                    }
+                    
+                    if (penonton[baris - 1][kolom - 1] != null) {
+                        System.out.println("mohon maaf, kursi sudah terisi");
+                        continue;
+                    }
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    penonton[baris - 1][kolom - 1] = nama;
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+        
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
+                case 2:
+
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.print("***\t");
+                            continue;
+                        }
+                        System.out.print(penonton[i][j]+"\t");
+                    }
+                    System.out.println(" ");
+                }
+                    break;
+                
+                case 3:
+                System.exit(0);
                 break;
             }
         }
